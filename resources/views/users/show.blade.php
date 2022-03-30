38ca2f47843dd3b460acb3f4129b417b68fa4dd1
charles3399 <bernaldezsay@gmail.com>
2022-03-30 12:25:29 +0800

    changed role to user label

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Role: {{$usersRoles->full_name}}
+            User: {{$usersRoles->full_name}}
         </h2>
     </x-slot>
 
